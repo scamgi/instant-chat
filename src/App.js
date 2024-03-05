c4c85b5f5538bdf3346c2a565fe3723ba2941df0
scamgi <scampini.giacomo@gmail.com>
2024-03-05 01:48:24 +0100

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-u93G1t88MkqFWL-hm1jRCiqL_gTnghU",
+  authDomain: "instant-chat-59328.firebaseapp.com",
+  projectId: "instant-chat-59328",
+  storageBucket: "instant-chat-59328.appspot.com",
+  messagingSenderId: "532947066136",
+  appId: "1:532947066136:web:5e2cb037a38ca611bf526d",
+  measurementId: "G-BHL00JNZ78"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
